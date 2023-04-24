6cd8563971e16f8dcf4f2a6e562d963bfbb391ba
patto220048 <220048xn@yse-c.net>
2023-04-24 10:07:13 +0900

    modai

--- src/sample/Condision.java
+++ src/sample/Condision.java
@@ -1,18 +1,26 @@
 package sample;
 
+import java.util.Calendar;
+
 public class Condision {
     public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        int week = cal.get(Calendar.DAY_OF_WEEK);
         String weekday = "";
+        if (week == 2 ){
+            weekday = "火"; 
+        };
         String garbage = "";
         switch (weekday) {
-            case "2":
-                garbage = "Monday";
+            case "月":
+                garbage = "燃えるゴミ";
                 break;
-            case "3":
-                garbage = "Tuesday";
+            case "火":
+                garbage = "燃えないゴミ";
                 break;
+        
             default:
-                garbage = "None";
+                garbage = "回収なし";
                 break;
         }
         System.out.println(garbage);
---
+++ src/sample/Mondai.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Mondai {
+    public static void main(String[] args) {
+        int age = 18;
+        boolean hasBankaccount = false;
+        boolean isBankList = false;
+        String mess = "クレジットを作成出来ません";
+
+        if (!isBankList && age >= 18 && age < 65) {
+            if (hasBankaccount) {
+                mess = "クレジットカードを出来ます";
+            } else {
+                mess = "必要！";
+            }
+        }
+        System.out.println(mess);
+    }
+}
