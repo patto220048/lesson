3b4b29362577f368f50f88903591b918ce074f12
patto220048 <220048xn@yse-c.net>
2023-05-22 10:12:53 +0900

    hashMap

--- src/shop/Drink.java
+++ src/shop/Drink.java
@@ -4,12 +4,15 @@ public class Drink {
     public String name;
     public int price;
     public int stock;
-    
     public Drink(String name, int price, int stock) {
         this.name = name;
         this.price = price;
         this.stock = stock;
     }
-
+    public Drink(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    
     
 }
--- src/shop/ListApp.java
+++ src/shop/ListApp.java
@@ -1,18 +1,23 @@
 package shop;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class ListApp {
     public static void main(String[] args) {
-        ArrayList<Drink> drinks  = new ArrayList<Drink>();
-         drinks.add(new Drink("コーヒー", 350, 100));
-         drinks.add(new Drink("紅茶", 400, 10));
-         drinks.add(new Drink("Coca", 300, 50));
-
-         for (Drink drink : drinks) {
+        Map<String, Drink> drinkMap = new HashMap<>();
+        drinkMap.put("D0001", new Drink("Milk", 350));
+        drinkMap.put("D0002", new Drink("Coca", 450));
+        drinkMap.put("D0003", new Drink("Pessi", 300));
+        for (Drink drink : drinkMap.values()) {
             System.out.println(drink.name);
-         }
-    }
-
+        }
+        Drink selecDrink = drinkMap.get("D0003");
+        System.out.println(selecDrink.name);
+        
+    }   
+    
     
 }
